eb3b6105bbdfebd11f61f1de07fa48accf94ceb1
KillerBossOriginal <2007.killerboss@gmail.com>
2022-01-20 21:35:58 +0100

    0.0.0.1

---
+++ handler/main.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+const Discord = discord;
+const config = require("../main.json")
+
+const client = new Discord.Client({intents: config.handler.intents});
+module.exports = {
+  run() {
+    client.login(config.handler.token);
+    return client;
+  },
+  get() {
+    return client;
+  },
+};
+
+require("./events.js")(client);
+/*
+const client = require('main.js').run(); // Per runnarlo e prenderlo
+const client = require('main.js').get(); // Per prenderlo e basta 
+*/
