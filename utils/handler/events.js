0dac3999c1362b63a72aeffe257159e729c76d1b
BlackdestinyXX <65021823+BlackdestinyXX@users.noreply.github.com>
2022-04-03 23:06:16 +0200

    Update events.js

--- utils/handler/events.js
+++ utils/handler/events.js
@@ -2,15 +2,16 @@ const { glob } = require("glob");
 const { promisify } = require("util");
 const { Client, Message } = require("discord.js");
 const globPromise = promisify(glob);
+const client = new Client()
 
 module.exports = async function (client) {
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
     eventFiles.map((value) => {
         const event = require(value)
         if (event.once) {
-	    	Client.once(event.name, (...args) => event.execute(...args));
+	    	client.once(event.name, (...args) => event.execute(...args));
 	    } else {
-		    Client.on(event.name, (...args) => event.execute(...args));
+		    client.on(event.name, (...args) => event.execute(...args));
     	}
   });
 }
